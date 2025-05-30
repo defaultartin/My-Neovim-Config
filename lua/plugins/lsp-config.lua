f481d7d26d60d2f3ce44f550f2c0b3df6023237d
Artin <artinkordzangeneh@gmail.com>
2025-05-30 11:42:31 +0330

    Opens Error panel when pressing 'E'

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -40,6 +40,7 @@ return {
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', 'E', vim.diagnostic.open_float, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }

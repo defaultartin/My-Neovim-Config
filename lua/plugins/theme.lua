962143ccbf754fdb5cea25b440da67961cdb8aea
Artin <artinkordzangeneh@gmail.com>
2025-07-09 18:54:50 +0330

    Added Vesper and Vague themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -119,3 +119,23 @@ return {
 		NoBackground()
     end
 }
+
+--[[return {
+    "vague2k/vague.nvim",
+    name = "vague",
+    priority = 1000,
+    config = function()
+        vim.cmd('colorscheme vague')
+		NoBackground()
+    end
+}--]]
+
+--[[return {
+    "datsfilipe/vesper.nvim",
+    name = "vesper",
+    priority = 1000,
+    config = function()
+        vim.cmd('colorscheme vesper')
+		NoBackground()
+    end
+}--]]

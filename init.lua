72d5e345641ecef6a91884bce221bcb72d4ec574
Artin <artinkordzangeneh@gmail.com>
2025-11-14 12:18:32 +0330

    little refactoring some plugins

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
-vim.cmd("set binary")
-vim.cmd("set noeol")
+-- Basic Settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.binary = true
+vim.opt.eol = false
+
+vim.opt.termguicolors = true
 
 -- highlight cursorline number
 vim.cmd("set cursorline")
 vim.cmd("set cursorlineopt=number")
-
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "Y", '"+y')
 
 vim.opt.smartindent = true
 vim.opt.hlsearch = false
@@ -24,39 +24,40 @@ vim.opt.scrolloff = 6
 --vim.cmd("set guicursor=n-c:hor100,v-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20")
 
 vim.g.mapleader = " "
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-vim.opt.termguicolors = true
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("v", "Y", '"+y')
+
 vim.keymap.set('n', '<leader>th', ':colorscheme ')
 
 
-
-
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-
-
-
-
-require("lazy").setup("plugins")
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = "plugins",
+	change_detection = { notify = false },
+})
